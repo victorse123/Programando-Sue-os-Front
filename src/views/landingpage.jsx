a6ea65b68acc9cbde46430ac903d08888572314a
Victor S <victorseva123@gmail.com>
2024-05-22 13:28:42 -0500

    Agrego footer a landingpage

--- src/views/landingpage.jsx
+++ src/views/landingpage.jsx
@@ -1,7 +1,36 @@
-    import styles from '../views/landingpage.module.css';
+import footer from '../components/footer/footer';
+import { useNavigate } from 'react-router-dom';
+import styles from '../views/landingpage.module.css';
 
-    const LandinPage = ( ) => {
+    const LandingPage = ( ) => {
+        const navigate = useNavigate()
+
+        return(
+             
+            <div>
+            <Footer />   
+
+       <div className={Styles.pagDiv}> 
+       
+
+           <video autoPlay playsInline muted loop>
+               <source src={imageImg.startVideo} type="video/mp4"/>
+           </video>
+           
+           <div className={Styles.imgs}>
+               <img className={Styles.startPag2} src={imageImg.LOGO} alt="" />
+               {allPokemon.length !== 0 && <img className={Styles.startPag} type='button' onClick={()=>navigate('/home')} src={imageImg.loanding} alt="" />}
+           </div>           
+              
+       </div>
+
+       
+
+       </div>
+   
+   )
 
     }
 
-    export default LandinPage;
+    export default LandingPage;
+
