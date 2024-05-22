b3a9c191d421634fcc2a9669a55ed9d95ec39387
Victor S <victorseva123@gmail.com>
2024-05-22 13:40:50 -0500

    actualizacion landingpage

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,18 @@
+// Importación de recursos y estilos
+//import React from "react";
+
+import './Footer.css'; // Estilos del Footer
+
+// Componente Funcional Footer
+function Footer() {
+  return (
+    <div className="Footer_component"> {/* Contenedor principal del Footer */}
+      <footer>
+        <span className="text_footer">Victor Sepulveda</span> {/* Texto del Footer */}
+        
+      </footer>
+    </div>
+  )
+}
+
+export default Footer; // Exportación del componente Footer
