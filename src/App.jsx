89ece3e80af26f1a18b9ca9bc0de0c743f5df2a4
Victor S <victorseva123@gmail.com>
2024-05-22 13:52:13 -0500

    establece ruta landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './views/landingpage';
 import './App.css'
 
 function App() {
@@ -8,7 +10,76 @@ function App() {
 
   return (
     <>
+     <BrowserRouter>
       <div>
+      <Routes>
+        <Route exact path='/' element ={<LandingPage/>}/>
+        
+      </Routes>
+      </div>
+    </BrowserRouter>
+    </>
+      
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+{/* <Route exact path='/home' element ={<Home/>}/>
+        <Route exact path='/home/:id' element ={<Detail/>}/>
+        
+        <Route exact path='/user/profile' element ={<UserProfile/>}/>
+        <Route exact path='/user/profile/edit' element ={<UserEditProfile/>}/>
+
+        <Route exact path='/create' element ={<ProtectedRoute Component={Create}/>}/>
+        <Route exact path='/admin/dashboard' element ={<ProtectedRoute Component={AdminDashboard}/>}/>
+        <Route exact path='/admin/editProperty/:id' element ={<ProtectedRoute Component={AdminEditProperty}/>}/>
+        <Route exact path='/admin/sales' element ={<ProtectedRoute Component={AdminSalesManagement}/>}/> */}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+{/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +99,4 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
-
-export default App
+    </> */}
