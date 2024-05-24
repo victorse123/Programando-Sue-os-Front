ecc5a3ea037a1a86f4ce12f0222f039939144e78
Victor S <victorseva123@gmail.com>
2024-05-24 11:49:09 -0500

    Actualizando funciones

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <h1>BIENVENIDOS</h1>
       <p>
         Hola, mi nombre es Víctor Alfonso Sepúlveda y me emociona demasiado poder presentarles 
-        a PROGRAMANDO SUEÑOS. Han sido días arduos de codeo, prueba y error; 
+        a PROGRAMANDO SUEÑOS. Se han tenido buenos dìas de codeo, prueba y error; 
         que hoy se ven reflejados en esta singlepage. Es una muestra de mi visión y lo que se 
         puede ofrecer cuando están en ella.
         Los comentarios siempre serán BIENVENIDOS y podrán agregar más valor a la app; para ello estamos 
